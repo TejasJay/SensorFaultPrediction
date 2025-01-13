efbb1a7bffe172450f9965f8485fa4bf46e466c1
Tejas Jay <79327536+TejasJay@users.noreply.github.com>
2025-01-12 23:01:17 -0500

    Update data_loader.py

--- data_ingestion/data_loader.py
+++ data_ingestion/data_loader.py
@@ -19,22 +19,36 @@ class Data_Getter:
         Method Name: get_data
         Description: This method reads the data from source.
         Output: A pandas DataFrame.
-        On Failure: Raise Exception
-
+        On Failure: Raise Exception with specific message.
+    
         Written By: Tejas Jay (TJ)
-        Version: 1.0
-        Revisions: None
-
+        Version: 1.1
+        Revisions: Added file validation and improved error handling.
+    
         """
-        self.logger_object.log(self.file_object,'Entered the get_data method of the Data_Getter class')
+        self.logger_object.log(self.file_object, 'Entered the get_data method of the Data_Getter class')
+        
+        # Validate the file path
+        if not self.training_file or not isinstance(self.training_file, str):
+            error_message = "Invalid file path provided. Please check the training file path."
+            self.logger_object.log(self.file_object, error_message)
+            raise Exception(error_message)
+    
         try:
-            self.data= pd.read_csv(self.training_file) # reading the data file
-            self.logger_object.log(self.file_object,'Data Load Successful.Exited the get_data method of the Data_Getter class')
+            # Log the file being used
+            self.logger_object.log(self.file_object, f'Reading data from file: {self.training_file}')
+            
+            # Reading the data file
+            self.data = pd.read_csv(self.training_file)
+            
+            self.logger_object.log(self.file_object, 'Data Load Successful. Exited the get_data method of the Data_Getter class')
             return self.data
+    
         except Exception as e:
-            self.logger_object.log(self.file_object,'Exception occured in get_data method of the Data_Getter class. Exception message: '+str(e))
-            self.logger_object.log(self.file_object,
-                                   'Data Load Unsuccessful.Exited the get_data method of the Data_Getter class')
-            raise Exception()
-
+            error_message = f"Exception occurred in get_data method of the Data_Getter class. Exception message: {str(e)}"
+            self.logger_object.log(self.file_object, error_message)
+            self.logger_object.log(self.file_object, 'Data Load Unsuccessful. Exited the get_data method of the Data_Getter class')
+            
+            # Raising the exception with a descriptive message
+            raise Exception(f"Data load failed: {str(e)}")
 
