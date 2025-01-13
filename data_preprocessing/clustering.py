02cd55e016645de8aa64cd824a96a61e45dbd42b
Tejas Jay <79327536+TejasJay@users.noreply.github.com>
2025-01-12 23:06:33 -0500

    Update clustering.py

--- data_preprocessing/clustering.py
+++ data_preprocessing/clustering.py
@@ -17,40 +17,72 @@ class KMeansClustering:
         self.file_object = file_object
         self.logger_object = logger_object
 
-    def elbow_plot(self,data):
+
+
+    def elbow_plot(self, data):
         """
         Method Name: elbow_plot
         Description: This method saves the plot to decide the optimum number of clusters to the file.
         Output: A picture saved to the directory and returns number of clusters
-        On Failure: Raise Exception
-
+        On Failure: Raise Exception with specific message.
+    
         Written By: Tejas Jay (TJ)
-        Version: 1.0
-        Revisions: None
-
+        Version: 1.1
+        Revisions: Added file path validation, enhanced logging, and improved exception handling.
         """
         self.logger_object.log(self.file_object, 'Entered the elbow_plot method of the KMeansClustering class')
-        wcss=[] # initializing an empty list
+    
+        # Validate the input data
+        if data is None or len(data) == 0:
+            error_message = "Input data is empty or None. Unable to perform elbow plot."
+            self.logger_object.log(self.file_object, error_message)
+            raise ValueError(error_message)
+    
+        wcss = []  # initializing an empty list to store Within-Cluster Sum of Squares (WCSS)
+    
         try:
-            for i in range (1,11):
-                kmeans=KMeans(n_clusters=i,init='k-means++',random_state=42) # initializing the KMeans object
-                kmeans.fit(data) # fitting the data to the KMeans Algorithm
+            for i in range(1, 11):
+                # Initializing the KMeans object
+                kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+                kmeans.fit(data)  # Fitting the data to the KMeans Algorithm
                 wcss.append(kmeans.inertia_)
-            plt.plot(range(1,11),wcss) # creating the graph between WCSS and the number of clusters
+                self.logger_object.log(self.file_object, f"WCSS for {i} clusters: {kmeans.inertia_}")
+    
+            # Plotting the WCSS values
+            plt.plot(range(1, 11), wcss)
             plt.title('The Elbow Method')
             plt.xlabel('Number of clusters')
             plt.ylabel('WCSS')
-            #plt.show()
-            plt.savefig('preprocessing_data/K-Means_Elbow.PNG') # saving the elbow plot locally
-            # finding the value of the optimum cluster programmatically
+    
+            # Check if the directory exists, else create it
+            save_dir = 'preprocessing_data'
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+                self.logger_object.log(self.file_object, f"Created directory: {save_dir}")
+    
+            # Saving the elbow plot locally
+            plot_path = os.path.join(save_dir, 'K-Means_Elbow.PNG')
+            plt.savefig(plot_path)
+            plt.close()  # Close the plot to free up memory
+    
+            self.logger_object.log(self.file_object, f'Elbow plot saved successfully at {plot_path}')
+    
+            # Programmatically find the optimal number of clusters using KneeLocator
             self.kn = KneeLocator(range(1, 11), wcss, curve='convex', direction='decreasing')
-            self.logger_object.log(self.file_object, 'The optimum number of clusters is: '+str(self.kn.knee)+' . Exited the elbow_plot method of the KMeansClustering class')
-            return self.kn.knee
-
+            optimum_clusters = self.kn.knee
+            self.logger_object.log(self.file_object, f'The optimum number of clusters is: {optimum_clusters}.')
+    
+            self.logger_object.log(self.file_object, 'Exited the elbow_plot method of the KMeansClustering class')
+            return optimum_clusters
+    
         except Exception as e:
-            self.logger_object.log(self.file_object,'Exception occured in elbow_plot method of the KMeansClustering class. Exception message:  ' + str(e))
-            self.logger_object.log(self.file_object,'Finding the number of clusters failed. Exited the elbow_plot method of the KMeansClustering class')
-            raise Exception()
+            error_message = f"Exception occurred in elbow_plot method of the KMeansClustering class. Exception message: {str(e)}"
+            self.logger_object.log(self.file_object, error_message)
+            self.logger_object.log(self.file_object, 'Finding the number of clusters failed. Exited the elbow_plot method of the KMeansClustering class')
+            
+            # Raising the exception with the specific error message
+            raise Exception(f"Failed to generate elbow plot or determine optimal clusters: {str(e)}")
+
 
     def create_clusters(self,data,number_of_clusters):
         """
