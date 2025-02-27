8e7b1aeeb6217964134d70e0b6bd288bf022fa4b
Tejas Jay <79327536+TejasJay@users.noreply.github.com>
2025-02-26 19:11:41 -0500

    Update DataTypeValidationPrediction.py

--- DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
+++ DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from os import listdir
 import os
 import numpy as np
-import pandas as pd
+import pandas
 import csv
 from application_logging.logger import App_Logger
 
